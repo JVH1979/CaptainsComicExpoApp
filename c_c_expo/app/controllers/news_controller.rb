3affa04fe6e1fee8fb9568286071f215192d800b
John Hammond <johnhammond1979@gmail.com>
2014-10-02 07:35:38 -0400

    CRUD for news and events

---
+++ c_c_expo/app/controllers/news_controller.rb
@@ -0,0 +1,35 @@
+class NewsController < ApplicationController
+  def new
+    @news = News.new
+  end
+
+  def create
+    @news = News.create news_params
+    redirect_to root_path
+  end
+
+  def edit
+    @news = News.find params[:id]
+  end
+
+  def update
+    @news = News.find params[:id]
+    @news.update_attributes news_params
+    redirect_to root_path
+  end
+
+  def show
+    @news = News.find params[:id]
+  end
+
+  def destroy
+    @news = News.find params[:id]
+    @news.delete
+    redirect_to root_path
+  end
+
+  private
+  def news_params
+    params.require(:news).permit(:headline, :story)
+  end
+end
